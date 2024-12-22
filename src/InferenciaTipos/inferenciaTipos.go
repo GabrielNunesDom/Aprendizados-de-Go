86e68b00c7ea6d043c867e31727bf73fc25f2c1e
gabriel <gabrielnunesdom@gmail.com>
2024-12-22 18:05:28 -0300

    Adicionando lições do dia 22/12/2024

---
+++ src/InferenciaTipos/inferenciaTipos.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nome = "Gabriel"
+	var idade = 29
+	var versao = 3.2
+	fmt.Println("Meu nome é", nome, "e minha idade é", idade)
+	fmt.Println("Estou usando o programa de versão:", versao)
+
+}
--- src/string/string.go
+++ src/string/string.go
@@ -3,8 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
-	fmt.Println("Hello World!")
 	fmt.Println(len("Hello World"))
 	fmt.Println("Hello World"[2])
 	fmt.Println("Hello" + "World")
